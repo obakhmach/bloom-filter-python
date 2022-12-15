eace9b8ca26674f37616ce10e1a7070878dd49a3
oleksander <alexandrbakhmach@gmail.com>
2022-12-15 18:47:33 +0200

    Add main frame files for the bloom filter class.

---
+++ bfilter/bloom.py
@@ -0,0 +1,75 @@
+import logging
+import math
+from functools import cached_property
+from typing import Optional, cast
+
+from bitarray import bitarray
+from mmh3 import hash as mhash
+from mmh3 import city_hash as chash
+
+
+class BloomFilter:
+    DEFAULT_FALSE_POSITIVE_PROBABILITY: float = 0.3
+
+    def __init__(self, items_count: int, false_positive_probability: Optional[float] = None) -> None:
+        """Constructor to init the bloom filter in most simplest form.
+
+        Args:
+            items_count: The amount of items planned to store in the bloom filter.
+            false_positive_probability: The probability of false positive responses from the
+                                        bloom filter.
+        """
+        self._items_added: int = 0
+        self._items_count: int = items_count
+        self._false_positive_probability: float
+        self._buffer: bitarray
+
+        if false_positive_probability is None:
+            self._false_positive_probability = self.DEFAULT_FALSE_POSITIVE_PROBABILITY
+
+        else:
+            self._false_positive_probability = false_positive_probability
+
+        # Because _number_of_bits is getter and it's value could be calculated ones
+        # the _false_positive_probability and _items_count where set.
+        self._buffer = bitarray(self._number_of_bits)
+
+    @cached_property
+    def _number_of_bits(self) -> int:
+        """A getter to calculate and receive the optimal
+        number of bits in the bloom filter bit array based on
+        desired _false_positive_probability and _items_count.
+
+        Returns:
+            The number of bits for bloom filter bit array.
+        """
+        return - (self._items_count * math.log(self._false_positive_probability, math.e)) / math.log(2, math.e) ** 2
+
+    @cached_property
+    def _number_of_hashes(self) -> int:
+        """Calculates the best number of hash functions used
+        to store the desired amount of items (_items_count).
+
+        Returns:
+            The best number of hash
+        """
+        return -math.log2(self._false_positive_probability)
+
+    def _calc_random_bit_array_index(self, item: str, seed: int) -> int:
+        """Calculates the bit to set in a bit array for a given item. 
+        This calculation per seed simulates the behavior 
+        of using unique hash function per a unique seed.
+        
+        Args:
+            item: An item we want to remember in the bloom filter.
+            seed: Any integer number.
+
+        Returns:
+            An index in bit array.
+        """
+        murmur_hash: int = mhash(item)
+        city_hash: int = chash(item)
+
+        aka_random_hash: int = murmur_hash + seed * city_hash
+
+        return aka_random_hash % self._number_of_bits
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="bfilter",
+    packages=find_packages(include=["bfilter"]),
+    version=version_string,
+    description="Python simple implementation for the bloom filter.",
+    author="alexandrbakhmachr@gmail.com",
+    license="MIT",
+    install_requires=["bitarray==2.6.0", "mmh3==3.0.0"],
+    setup_requires=["pytest-runner"],
+    tests_require=[
+        "pytest==7.1.3",
+        "mmh3==3.0.0",
+        "bitarray==2.6.0",
+    ],
+    test_suite="tests",
+    python_requires=">=3.10",
+)
